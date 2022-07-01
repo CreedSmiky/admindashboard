58dc37b492b9ca91f7566da5d036e079c5dd7778
CreedSmiky <mwitichris99@gmail.com>
2022-07-01 11:28:29 +0300

    updates

--- index.php
+++ index.php
@@ -121,7 +121,7 @@
 
 
 
-    <script src="/bootstrap-5.2.0/bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js"></script>
-    <script src="/bootstrap-5.2.0/bootstrap-5.1.3-dist/js/bootstrap.min.js"></script>
+    <script src="bootstrap-5.2.0/bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js"></script>
+    <script src="bootstrap-5.2.0/bootstrap-5.1.3-dist/js/bootstrap.min.js"></script>
 </body>
 </html>
