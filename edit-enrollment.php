4e027255bbcb771488002a45fd70fa0ad51f0070
CreedSmiky <mwitichris99@gmail.com>
2022-07-07 11:39:16 +0300

    updates on edit-enrollment edit options

--- edit-enrollment.php
+++ edit-enrollment.php
@@ -12,6 +12,10 @@
 
     {
         $fullname=$fetchStudent['fullname'];
+        $phonenumber=$fetchStudent['phonenumber'];
+        $email=$fetchStudent['email'];
+        $gender= $fetchStudent['gender'];
+        $course= $fetchStudent['course'];
 
     }
 ?>
@@ -31,8 +35,73 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="card">
-                        <div class="card-header bg-dark text-white text-center">
-                            <h4>Edit  <?php echo $fullname ?></h4>
+                            <div class="card-header bg-dark text-white text-center">
+                                <h4>Edit  <?php echo $fullname ?></h4>
+                            </div>
+                        <div class="card-body">
+                            <form action="edit-enrollment.php" method="POST">
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        <label for="full name" class="form-label"><b>Full Name:</b></label>
+                                        <input type="text" class="form-control" id="full name" placeholder="Enter your full name" name="fullname" value="<?php echo $fullname ?>">
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <label for="phone" class="form-label"><b>Phone number:</b></label>
+                                        <input type="tel" class="form-control" id="phone" placeholder="+2547...." name="phone" value="<?php echo $phonenumber ?>">
+                                    </div>
+                                </div>
+                    
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        <label for="email" class="form-label"><b>Email address:</b></label>
+                                        <input type="email" class="form-control" id="email" placeholder="Enter your email" name="email" value="<?php echo $email?>">
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <label for="gender" class="form-label"><b>What is your gender:</b></label>
+                                        <select class="form-select" aria-label="Default select example" name="gender">
+                                            <option><?php echo $gender ?></option>
+                                            <option value="Male">Male</option>
+                                            <option value="Female">Female</option>
+                                        </select> 
+                                    </div>
+                                </div>
+                                <br>
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                    <p>In order to complete your information to the bootcamp, you are required to select one course you will be<br>undertaking. Please NOTE that this will be your learning track during the 2-weeks immersion.</p>
+                                    </div>
+                                </div>
+                                <br>
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        <label for="course" class="form-label"><b>What is your preffered course ?</b></label>
+                                        <select class="form-select" aria-label="Default select example" name="course">
+                                            <option><?php echo $course ?></option>
+                                            <option value="Web development">Web development</option>
+                                            <option value="Cyber security">Cyber security</option>
+                                        </select>
+                                    </div>
+                            
+                                </div>
+                                <br>
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                        <p>You agrre by providing your information you understand all our data privacy an protection policy<br>outlined in our Terms$ condition and the Privacy Policy statements.</p>
+                                    </div>
+                                </div>
+                                <br>
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                        <input type="checkbox" id="checkbox">
+                                        <label for="checkbox"><b>Agree terms and condition</b></label>
+                                        
+                                    </div>
+                                </div>
+                                <br>
+                                <button type="submit" name="Submitbutton" class="btn btn-primary mb-3">submit application</button> 
+                            </form> 
+                            
+                            
                         </div>
                     </div>
                 </div>
